d92a4f7760cc9ac6886ab48e011b653d15435756
Jeremiah Bell <jeremiah.bell812.topper@gmail.com>
2019-10-10 18:23:23 -0600

    Initial push made js file and am attempting to refator for Chicken Monkey

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+for (let currentNumber = 1; currentNumber <= 100; currentNumber++) {
+
+    // Divide the current number by 2, and check if the remainder is 0
+    if (currentNumber % 5 === 0) {
+        console.log("Chicken") // Only 2, 4, 6 will appear
+
+    } else if (currentNumber % 7 === 0)
+    console.log("Monkey")
+
+} else (currentNumber % 5 === 0)
+    console.log("Chicken")
+    }
+
+
